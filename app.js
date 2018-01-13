5cf05328733a84a8d01dc8b00e959af99785b790
gmanjames <yms1465@gmail.com>
2018-01-13 14:00:55 -0600

    Added some basic back and forth communication.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+'use strict';
+
 const express = require('express'),
       app     = express(),
-      server  = require('http').createServer(app);
+      server  = require('http').createServer(app),
       io      = require('socket.io')(server);
 
 
@@ -9,6 +11,48 @@ app.use(express.static('./public'));
 
 
 // Start up app listening on port
-app.listen('3000');
+server.listen('3000');
+
+
+// Application
+let numUsers = 0;
+
+io.on('connection', socket => {
+    let addedUser = false;
+
+    socket.on('add-user', dat => {
+        if (numUsers === 2
+        ||  addedUser) {
+            return;
+        }
+
+        addedUser = true;
+        numUsers++;
+
+        socket.emit('login', {
+            name  : dat,
+            color : numUsers === 1 ? 'black' : 'white'
+        });
+    }); // end on.add-user
+
+    socket.on('try-move', dat => {
+        let legal = true;
+
+        if (legal) {
+            socket.emit('move', { legal: true, pos: {x: dat.x, y: dat.y}, image: dat.image });
+
+            socket.broadcast.emit('update-board', {
+                color   : dat.color,
+                image   : dat.image,
+                results : [],
+                pos     : {x: dat.x, y: dat.y}
+            });
+        }
+    });
+});
+
+io.on('disconnect', socket => {
+    if (numUsers > 0) numUsers--;
+});
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,69 @@
 'use strict';
 
-const socket = io();
+const socket   = io(),
+      submit   = document.getElementById('submit'),
+      username = document.getElementById('username');
+
+let user;
 
 
-let numUsers = 0;
+function handleMove(evt) {
+    const point  = evt.target;
+    let [x, y] = point.dataset.pos.split(','),
+        image;
+
+    x = parseInt(x);
+    y = parseInt(y);
+
+    if ((x === 1 && x === y)
+    ||  (x === 1 && y === 9)
+    ||  (x === 9 && y === 1)
+    ||  (x === 9 && x === y))
+        image = 'corner';
+
+    else if (x > 1 && x < 9 && y > 1 && y < 9)
+        image = 'middle';
+
+    else
+        image = 'side';
+
+    socket.emit('try-move', {x: x, y: y, color: user.color, image: image});
+}
+
+document.querySelectorAll('.point').forEach(item => {
+    item.addEventListener('click', handleMove);
+});
+
+submit.addEventListener('click', evt => {
+    evt.preventDefault();
+
+    socket.emit('add-user', username.value);
+});
+
+socket.on('login', dat => {
+    user = dat;
+    document.body.append(JSON.stringify(dat));
+});
+
+socket.on('move',  dat => {
+    console.log(dat);
+
+    if (dat.legal) {
+        let posdat = dat.pos.x + ',' + dat.pos.y,
+            color  = user.color === 'black' ? 'b' : 'w',
+            point  = document.querySelector(".point[data-pos='" + posdat + "']");
+
+        point.style.backgroundImage = "url('/images/i-" + dat.image + "-" + color + ".png')";
+
+    }
+});
+
+socket.on('update-board', dat => {
+    console.log(dat);
+
+    let posdat = dat.pos.x + ',' + dat.pos.y,
+        color  = dat.color === 'black' ? 'b' : 'w',
+        point  = document.querySelector(".point[data-pos='" + posdat + "']");
+
+    point.style.backgroundImage = "url('/images/i-" + dat.image + "-" + color + ".png')";
+});
